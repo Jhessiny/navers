eb4c2aa9adc114500f32db5c59d77c28f86bef55
jhessiny <jhessinymattos@gmail.com>
2021-03-14 16:17:04 -0300

    styling confirmation modal

--- src/components/ModalContents/DeletingModalContent.jsx
+++ src/components/ModalContents/DeletingModalContent.jsx
@@ -3,11 +3,12 @@ import React from "react";
 function DeletingModalContent({ toggleModal, deleteNaver, deletingUserId }) {
   return (
     <div className="modal__card">
-      <i className="close fas fa-times" onClick={() => toggleModal()}></i>
       <h2>Excluir Naver</h2>
       <p>Tem certeza que deseja excluir este Naver?</p>
       <div className="delete-modal__btns-wrapper">
-        <button onClick={() => toggleModal()}>Cancelar</button>
+        <button className="btn-border" onClick={() => toggleModal()}>
+          Cancelar
+        </button>
         <button onClick={() => deleteNaver(deletingUserId)}>Excluir</button>
       </div>
     </div>
